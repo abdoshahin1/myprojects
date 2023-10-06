d2655b012c3a14ccce3ce50f93cabcbc56be79d4
abdoshahin1 <abdoahmadm55@gmail.com>
2023-10-06 17:43:36 +0300

    i optimize this app again

--- py_projects/test_web_scraping.py
+++ py_projects/test_web_scraping.py
@@ -27,7 +27,6 @@ test = web.find_element(By.CSS_SELECTOR, "#q")
 test.send_keys(f"{user_input}", Keys.ARROW_DOWN)
 web.find_element(By.CLASS_NAME, "search").click()
 web.find_element(By.CSS_SELECTOR, "body > div.container > div.containerBlock > div.oneResult > a").click()
-
 for i in range(1, 7):
     prayer = web.find_element(By.CSS_SELECTOR, f"body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child({i})").text
     prayer_times.append(prayer)
@@ -35,6 +34,7 @@ with open(f"{path_file}", "w", encoding="utf-8") as f:
     for prayer_time in prayer_times:
         f.write(prayer_time)
         f.write("\n\n")
+
 f.close()
 web.close()
 print(f"to open the file copy this path =>{path_file} and paste it in path search box.")
