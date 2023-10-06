bd7376e886e757506b74ab8841e2952b84c8b88b
abdoshahin1 <abdoahmadm55@gmail.com>
2023-10-06 17:42:20 +0300

    i optimize this app again

--- py_projects/test_web_scraping.py
+++ py_projects/test_web_scraping.py
@@ -12,7 +12,10 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 def path ():
     file_path = input("enter the path: ").strip()
-    file_path = r"C:\Users\Public\Downloads\prayer_times.txt"
+    if file_path == "":
+        file_path = r"C:\Users\Public\Downloads\prayer_times.txt"
+    else:
+        file_path = file_path + r"\prayer_times.txt"
     return file_path
 path_file = path()
 user_input = input("enter your governorate: ").strip().capitalize()
@@ -20,20 +23,13 @@ prayer_times = []
 
 web = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
 web.get("https://timesprayer.com/prayer-times-cities-egypt.html")
-css_selector = ["body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child(1)", 
-                "body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child(2)", 
-                "body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child(3)", 
-                "body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child(4)", 
-                "body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child(5)", 
-                "body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child(6)"]
-
 test = web.find_element(By.CSS_SELECTOR, "#q")
 test.send_keys(f"{user_input}", Keys.ARROW_DOWN)
 web.find_element(By.CLASS_NAME, "search").click()
 web.find_element(By.CSS_SELECTOR, "body > div.container > div.containerBlock > div.oneResult > a").click()
 
-for i in range(len(css_selector)):
-    prayer = web.find_element(By.CSS_SELECTOR, css_selector[i]).text
+for i in range(1, 7):
+    prayer = web.find_element(By.CSS_SELECTOR, f"body > div.container > div.containerBlock.prayerBlock > div > div.prayerDes > div > div:nth-child(1) > table > tbody > tr:nth-child({i})").text
     prayer_times.append(prayer)
 with open(f"{path_file}", "w", encoding="utf-8") as f:
     for prayer_time in prayer_times:
@@ -41,3 +37,4 @@ with open(f"{path_file}", "w", encoding="utf-8") as f:
         f.write("\n\n")
 f.close()
 web.close()
+print(f"to open the file copy this path =>{path_file} and paste it in path search box.")
