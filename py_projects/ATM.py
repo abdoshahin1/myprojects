b3713ececcef69cc0b408f4349a8499b3d502801
abdoshahin1 <abdoahmadm55@gmail.com>
2023-10-27 18:13:43 +0200

    uploading

--- py_projects/ATM.py
+++ py_projects/ATM.py
@@ -2,6 +2,9 @@ import os
 import time
 import sqlite3
 
+# clear the screen on terminal
+def clear() -> None:
+    os.system("cls")
 # create database and set up the cursor and commit the changes
 db = sqlite3.connect("ATM.db")
 cr = db.cursor()
@@ -13,22 +16,59 @@ class Account:
     num_user = 1
     @classmethod
     def new_account(cls) -> None:
-        name = input("enter your name: ").strip().capitalize()
-        address = input("enter your address: ").strip().capitalize()
+        time.sleep(0.7)
+        clear()
+        name = input("Enter your name: ").strip().capitalize()
+        address = input("Enter your address: ").strip().capitalize()
         while name == "" or address == "":
-            print("please enter the priviest field.")
-            name = input("enter your name: ").strip().capitalize()
-            address = input("enter your address: ").strip().capitalize()
-        age = int(input("enter your age: ").strip())
+            print("Please enter the priviest field.")
+            name = input("Enter your name: ").strip().capitalize()
+            address = input("Enter your address: ").strip().capitalize()
+        age = int(input("Enter your age: ").strip())
         if age < 18:
-            print("you are underage, you do not pass the allowed limit for age.")
+            print("You are underage, you do not pass the allowed limit for age.")
         else:
-            email = input("enter your email: ").strip().capitalize()
-            password = input("enter the password: ").strip()
-            confirm_pass = input("confirm the pass: ").strip()
+            email = input("Enter your email: ").strip().capitalize()
+            password = input("Enter the password: ").strip()
+            confirm_pass = input("Confirm the pass: ").strip()
             while confirm_pass != password:
-                print("please enter same password.")
-                confirm_pass = input("confirm the pass: ").strip()
-        details = (Account.num_user, name, address, age, email, password)
-        cr.execute("insert into users values(?,?,?,?,?,?)", details)
-        
+                print("Please enter same password.")
+                confirm_pass = input("Confirm the pass: ").strip()
+            details = (Account.num_user, name, address, age, email, password)
+            cr.execute("select * from users")
+            all_data = cr.fetchall()
+            cr.execute("insert into users values(?,?,?,?,?,?)", details)
+            if len(all_data) != 0:
+                Account.num_user = all_data[-1][0] + 1
+                cr.execute(f"update users set User_id = {Account.num_user} where Name = '{name}' ")
+            save_info()
+            print("Loading........")
+            time.sleep(1.5)
+            print("Account is created.")
+    def logging(cls) -> None:
+        pass
+    def start():
+        welcome_message = """Hello my dear client this is a simple ATM application you can use is easily:
+        1- Create a new account.
+        2- Login into your account.
+        3- Exit."""
+        print(welcome_message)
+        try:
+            option = int(input("Enter the option: ").strip())
+            while option not in [1, 2, 3]:
+                print("Please Enter the right option.")
+                option = int(input("Enter the option: ").strip())
+            else:
+                if option == 1:
+                    Account.new_account()
+                elif option == 2:
+                    pass
+                elif option == 3:
+                    pass
+        except ValueError:
+            clear()
+            print("Only number allowed, Please enter option again.")
+            time.sleep(1)
+            Account.start()
+# start the program
+Account.start()
