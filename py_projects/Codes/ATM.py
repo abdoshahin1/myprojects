6894dfefb89688591fad6b844a27f3348f880e61
abdoshahin1 <abdoahmadm55@gmail.com>
2024-02-07 19:14:10 +0200

    optimization

--- py_projects/Codes/ATM.py
+++ py_projects/Codes/ATM.py
@@ -9,15 +9,10 @@ def clear() -> None:
 # create database and set up the cursor and commit the changes
 db = sqlite3.connect("ATM.db")
 cr = db.cursor()
-cr.execute("create table if not exists users (User_id integer, Name text, Address text, Age integer, Email text, Password text)")
-
-welcome_message = """Hello my dear client this is a simple ATM application you can use:
-    1- Create a new account.
-    2- Login into your account.
-    3- Exit."""
+cr.execute("create table if not exists users (User_id integer, Name text, Address text, Age integer, Email text, Password text, Money integer)")
 class Account:
+    user_pass= 0
     num_user = 1
-    money = 0
     @classmethod
     def new_account(cls) -> None:
         sleep(1)
@@ -38,11 +33,18 @@ class Account:
             while confirm_pass != password:
                 print("Please enter same password.")
                 confirm_pass = input("Confirm the pass: ").strip()
-            details = (Account.num_user, name, address, age, email, password)
+            try:
+                money = int(input("Enter Money: ").strip())
+                print("done added money.")
+            except ValueError:
+                print("please, enter number only.")
+                money = int(input("Enter Money: ").strip())
+                print("done added money.")
+            details = (Account.num_user, name, address, age, email, password, money)
             cr.execute("select * from users")
             all_data = cr.fetchall()
             if all_data != []:
-                cr.execute("insert into users values(?,?,?,?,?,?)", details)
+                cr.execute("insert into users values(?,?,?,?,?,?,?)", details)
                 Account.num_user = all_data[-1][0] + 1
                 cr.execute(f"update users set User_id = {Account.num_user} where Name = '{name}' ")
                 db.commit()
@@ -52,7 +54,7 @@ class Account:
                 sleep(1)
                 clear()
             else:
-                cr.execute("insert into users values(?,?,?,?,?,?)", details)
+                cr.execute("insert into users values(?,?,?,?,?,?,?)", details)
                 db.commit()
                 print("Loading........")
                 sleep(2)
@@ -71,6 +73,7 @@ class Account:
             list_details.append(data[num][0])
             list_details.append(data[num][1])
         if confirm_email in list_details and confirm_password in list_details:
+            Account.user_pass = confirm_password 
             print("loading..........")
             sleep(0.6)
         else:
@@ -79,55 +82,70 @@ class Account:
             Account.logging()
     @classmethod
     def details(cls) -> None:
-        clear()
-        cr.execute("select User_id, Password from users")
+        cr.execute("select * from users")
         details = cr.fetchall()
+        for M in range(len(details)):
+            if Account.user_pass in details[M]:
+                all_info = details[M]
+                new_money = details[M][-1]
         list_message = """ -------------------menu-------------------
             1 => Menu
-            2 => Change password
-            3 => Deposit
-            4 => Withdraw 
-            5 => Money transfer
-            6 => Total money
-            7 => Exit
-Enter the option: """
+            2 => Display info
+            3 => Change password
+            4 => Deposit
+            5 => Withdraw 
+            6 => Money transfer
+            7 => Total money
+            8 => Exit
+    Enter the option: """
         try:
             option = int(input(list_message).strip())
         except ValueError:
-            clear()
             print('enter the number only'.capitalize())
             sleep(2)
             Account.details()
         if option == 1:
+            clear()
             Account.details()
         elif option == 2:
+            for info in range(len(all_info)):
+                print(f"""-----------------My Information-----------------
+                    Name => {all_info[1]}
+                    Address => {all_info[2]}
+                    Age => {all_info[3]}
+                    Email => {all_info[4]}
+                    Password => {all_info[5]}
+                    Money => {all_info[6]}""")
+                break
+            Account.details()
+        elif option == 3:
             clear()
             pass_change = input("Enter old password:").strip()
-            for i in range(len(details)):
-                if pass_change in details[i] : 
-                    pass_change = input("Enter new password: ").strip()
+            if pass_change == Account.user_pass : 
+                pass_change = input("Enter new password: ").strip()
+                confirm_pass_change = input("Confirm password: ").strip()
+                if confirm_pass_change == pass_change:
+                    cr.execute(f"update users set Password = '{pass_change}' where Password = '{Account.user_pass}'")
+                else:
+                    print("please, enter the same password.")
                     confirm_pass_change = input("Confirm password: ").strip()
                     if confirm_pass_change == pass_change:
-                        cr.execute(f"update users set Password = '{pass_change}' where User_id = {details[i][0]}")
-                    else:
-                        print("please, enter the same password.")
-                        confirm_pass_change = input("Confirm password: ").strip()
-                        if confirm_pass_change == pass_change:
-                            cr.execute(f"update users set Password = '{pass_change}' where User_id = {details[i][0]}")
+                        cr.execute(f"update users set Password = '{pass_change}' where Password = '{Account.user_pass}'")
             db.commit()
             print("Your password is updated.")
             Account.details()
-        elif option == 3:
+        elif option == 4:
             input_money = int(input("Enter your money: ").strip())
-            Account.money = Account.money + input_money
+            cr.execute(f"update users set Money = {new_money + input_money} where Password = '{Account.user_pass}'")
+            db.commit()
             print("money is deposited")
             sleep(0.9)
             Account.details()
-        elif option == 4:
+        elif option == 5:
             try:
                 input_money = int(input("Enter your money: ").strip())
-                if input_money < Account.money:
-                    Account.money = Account.money - input_money
+                if input_money < new_money:
+                    cr.execute(f"update users set Money = {new_money - input_money} where Password = '{Account.user_pass}'")
                     print("money is withdraw")
                     sleep(0.9)
                     Account.details()
@@ -135,18 +153,19 @@ Enter the option: """
                 print("""You have money less than that you entered.
                         please, enter money again.""")
                 input_money = int(input("Enter your money: ").strip())
-                if input_money < Account.money:
-                    Account.money = Account.money - input_money
+                if input_money < new_money:
+                    cr.execute(f"update users set Money = {new_money - input_money} where Password = '{Account.user_pass}'")
                     print("money is withdraw")
+                    db.commit()
                     sleep(0.9)
                     Account.details()
-        elif option == 5:
-            pass
         elif option == 6:
-            print(f"your money is {Account.money} $.")
+            pass
+        elif option == 7:
+            print(f"your money is {new_money}$.")
             sleep(3)
             Account.details()
-        elif option == 7:
+        elif option == 8:
             start()
         else:
             print("please enter the correct option.")
@@ -155,7 +174,10 @@ Enter the option: """
 # start the program
 def start():
         clear()
-        print(welcome_message)
+        print("""Hello my dear client this is a simple ATM application you can use:
+    1- Create a new account.
+    2- Login into your account.
+    3- Exit.""")
         try:
             option = int(input("Enter the option: ").strip())
             while option not in [1, 2, 3]:
